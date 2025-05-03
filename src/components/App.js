d95828903e275bff6aad198a4f71150a7a58f438
Brian <brian.sila1@student.moringaschool.com>
2025-05-03 17:14:10 +0300

    added filter and sort functionality

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,48 @@ import Hogcard from "./Hogcard";
 
 function App() {
   const [showDetails, setShowDetails] = useState(null);
+  const [ShowGreased, setShowGreased] = useState(false);
+  const [SortBy, setSortBy] = useState("none");
 
   function handleshow(hogName) {
     setShowDetails((prevName) => (prevName === hogName ? null : hogName));
   }
 
+  const filteredhogs = ShowGreased ? hogs.filter((hog) => hog.greased) : hogs;
+
+  const sortedHogs = [...filteredhogs].sort((a, b) => {
+    if (SortBy === "name") {
+      return a.name.localeCompare(b.name);
+    } else if (SortBy === "weight") {
+      return a.weight - b.weight;
+    }
+    return 0;
+  });
+
   return (
     <div className="App">
       <Nav />
-      <Hogcard hogs={hogs} handleshow={handleshow} showDetails={showDetails} />
+      <Hogcard
+        hogs={sortedHogs}
+        handleshow={handleshow}
+        showDetails={showDetails}
+      />
+      <label>
+        Show Greased Hogs Only:
+        <input
+          type="checkbox"
+          checked={ShowGreased}
+          onChange={() => setShowGreased(!ShowGreased)}
+        />
+      </label>
+      <label>
+        Sort by:
+        <select value={SortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="none">None</option>
+          <option value="name">Name</option>
+          <option value="weight">Weight</option>
+        </select>
+      </label>
     </div>
   );
 }
